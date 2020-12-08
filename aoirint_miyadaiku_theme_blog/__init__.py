51874cde2c71f2bb8379cb41c289b276939fa49e
aoirint <aoirint@gmail.com>
2020-12-08 13:17:15 +0900

    rm sitebar toggle

--- aoirint_miyadaiku_theme_blog/__init__.py
+++ aoirint_miyadaiku_theme_blog/__init__.py
@@ -1,1 +1,1 @@
-__version__ = '0.1.13.post1'
+__version__ = '0.1.13.post2'
--- aoirint_miyadaiku_theme_blog/files/static/aoirint-blog/js/main.js
+++ aoirint_miyadaiku_theme_blog/files/static/aoirint-blog/js/main.js
@@ -1,6 +1,5 @@
 
 const SMARTPHONE_VISIBLE_ATTR = 'data-smartphone-visible';
-const SITEBAR_QUERY = '.sitebar-box';
 const TOCBAR_QUERY = '.tocbar-box';
 
 function hideElementIfSmartphone(element) {
@@ -19,14 +18,8 @@ function toggleElementIfSmartphone(element) {
 }
 
 function hideBarsIfSmartphone() {
-  hideElementIfSmartphone(document.querySelector(SITEBAR_QUERY));
-  hideElementIfSmartphone(document.querySelector(TOCBAR_QUERY));
-}
-function toggleSiteBarIfSmartphone() {
-  toggleElementIfSmartphone(document.querySelector(SITEBAR_QUERY));
   hideElementIfSmartphone(document.querySelector(TOCBAR_QUERY));
 }
 function toggleTocBarIfSmartphone() {
   toggleElementIfSmartphone(document.querySelector(TOCBAR_QUERY));
-  hideElementIfSmartphone(document.querySelector(SITEBAR_QUERY));
 }
